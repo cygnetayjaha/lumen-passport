4c20c3e3a078beaad3a0ea6dd5124b8206861ca2
Anuj Jaha <anuj.jaha@cygnetinfotech.com>
2022-03-10 12:33:03 +0530

    Package Initial Version

---
+++ src/Entities/Traits/AccessTokenTrait.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @author      Alex Bilbie <hello@alexbilbie.com>
+ * @copyright   Copyright (c) Alex Bilbie
+ * @license     http://mit-license.org/
+ *
+ * @link        https://github.com/thephpleague/oauth2-server
+ */
+
+namespace Cygnet\OAuth2\Server\Entities\Traits;
+
+use DateTime;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Signer\Rsa\Sha256;
+use Lcobucci\JWT\Token;
+use Cygnet\OAuth2\Server\CryptKey;
+use Cygnet\OAuth2\Server\Entities\ClientEntityInterface;
+use Cygnet\OAuth2\Server\Entities\ScopeEntityInterface;
+use Illuminate\Http\Request;
+
+trait AccessTokenTrait
+{
+    /**
+     * Generate a JWT from the access token
+     *
+     * @param CryptKey $privateKey
+     *
+     * @return Token
+     */
+    public function convertToJWT(CryptKey $privateKey)
+    {
+        $requestInput   = app('request');
+        $customPayload  = [];
+
+        if(isset($requestInput) 
+            && isset($requestInput['custom_data'])
+            && !empty($requestInput['custom_data'])
+        )
+        {
+            $customPayload = $requestInput['custom_data'];
+        }
+
+        return (new Builder())
+            ->setAudience($this->getClient()->getIdentifier())
+            ->setId($this->getIdentifier(), true)
+            ->setIssuedAt(time())
+            ->setNotBefore(time())
+            ->setExpiration($this->getExpiryDateTime()->getTimestamp())
+            ->setSubject($this->getUserIdentifier())
+            ->set('scopes', $this->getScopes())
+            ->set('custom_data', $customPayload)
+            ->sign(new Sha256(), new Key($privateKey->getKeyPath(), $privateKey->getPassPhrase()))
+            ->getToken();
+    }
+
+    /**
+     * @return ClientEntityInterface
+     */
+    abstract public function getClient();
+
+    /**
+     * @return DateTime
+     */
+    abstract public function getExpiryDateTime();
+
+    /**
+     * @return string|int
+     */
+    abstract public function getUserIdentifier();
+
+    /**
+     * @return ScopeEntityInterface[]
+     */
+    abstract public function getScopes();
+}
